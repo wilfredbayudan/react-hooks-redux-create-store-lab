26200af3a7f425d97fe2b570e4a274c1641b15a8
Wilfred <wilfredbayudan@gmail.com>
2022-03-09 20:47:49 -1000

    commit tree 1625bb1f02786c213af925bc02f5a420c60ef74e

--- src/index.js
+++ src/index.js
@@ -1,7 +1,22 @@
 // Note: createStore and candyReducer must be exported for the tests to run
 
-export function createStore() {
+export function createStore(reducer) {
   // write your createStore code here
+  let state;
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    render();
+  };
+
+  function getState() {
+    return state;
+  };
+
+  return {
+    dispatch,
+    getState
+  };
 }
 
 export function candyReducer(state = [], action) {
@@ -24,3 +39,6 @@ function render() {
 
 // Use your createStore function and the functions provided here to create a store.
 // Once the store is created, call an initial dispatch.
+
+let store = createStore(candyReducer);
+store.dispatch({ type: "@@INIT" })
